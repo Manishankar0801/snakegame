13c9eb9a199c5dbb7e4eb543ab116680fbf53414
Mani Shankar Janumpalli <shankarmani1432@gmail.com>
2024-10-17 01:55:26 -0700

    snake game

---
+++ script.js
@@ -0,0 +1,15 @@
+let highscore = 0;
+let currscore = 0;
+let snake = [
+  { x: 10, y: 10 },
+  { x: 10, y: 11 },
+  { x: 10, y: 12 },
+];
+
+let food = [{ x: 14, y: 14 }];
+
+let hasGameStarted = false;
+
+const drawSnake = () => {};
+const generateFood = () => {};
+const move = (dir) => {};
